62ec540706d08761cc0328512bf25f21b45880d3
Kagaline <809Kagaline@users.noreply.github.com>
2023-03-03 21:33:06 +0900

    add game status loop function

--- shadowBot/shadowBot.py
+++ shadowBot/shadowBot.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import os
 import asyncio
 from typing import Union
+from itertools import cycle
 
 
 # intentの作成(全て許可)
@@ -13,6 +14,7 @@ shadowBot = commands.Bot(
     intents=discord_intents,
     case_insensitive=True
 )
+
 
 # 実験用サーバのサーバID(Guild ID)を取得⇒ギルド情報を取得
 with open('guild_id.txt', 'r') as guild_id_file:
@@ -31,11 +33,19 @@ async def update_member_cache() -> None:
         print(member.name)
     await guild.chunk()
 
+
+
+status = cycle(["command: ping", "command: developer", "command: contact"])
+@tasks.loop(seconds=5)
+async def change_status()->None:
+    await shadowBot.change_presence(activity=discord.Game(next(status)))
+
 # ボットが起動したときの処理
 @shadowBot.event
 async def on_ready() -> None:
     print("shadowBot starts working!")
     update_member_cache.start()
+    change_status.start()
 
 # Cogをロードする
 async def load() -> None:
